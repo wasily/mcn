99f1a3d728df99cc43618bff212deacc0b8377bb
wasily <zaharov_vs@mail.ru>
2020-07-25 13:53:54 +0300

    clean up

--- src/main/java/me/local/mcn/repositories/SubscriptionRepository.java
+++ src/main/java/me/local/mcn/repositories/SubscriptionRepository.java
@@ -1,6 +1,5 @@
 package me.local.mcn.repositories;
 
-import me.local.mcn.domain.Movie;
 import me.local.mcn.domain.model.Subscription;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
