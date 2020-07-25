da07209f63a21e791f442f4981237f6c43e3ead2
wasily <zaharov_vs@mail.ru>
2020-07-25 09:23:54 +0300

    model added

---
+++ src/main/java/me/local/mcn/domain/model/Release.java
@@ -0,0 +1,29 @@
+package me.local.mcn.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "releases")
+public class Release {
+    @Field(value = "title")
+    private String title;
+
+    @Field(value = "size")
+    private long size;
+
+    @Field(value = "infoHash")
+    private String infoHash;
+
+    @Field(value = "releaseTime")
+    private LocalDateTime regTime;
+}
---
+++ src/main/java/me/local/mcn/domain/model/Subscription.java
@@ -0,0 +1,26 @@
+package me.local.mcn.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document(collection = "subscriptions")
+public class Subscription {
+    @Field(value = "title")
+    private String title;
+
+    @Field(value = "email")
+    private String email;
+
+    @Field(value = "lastUpdateTime")
+    private LocalDateTime lastUpdateTime;
+}
